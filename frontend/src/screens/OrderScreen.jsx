2c4fc12671a2f8ef7e02dfa87c2c424663005e75
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-23 22:47:24 -0700

    Rebase: PayPal Buttons

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -169,9 +169,9 @@ const OrderScreen = () => {
 
                   {isPending ? <Loader/> : (
                     <div>
-                      <Button onClick={onApproveTest} style={{marginBottom: "10px"}}>
+                      {/*<Button onClick={onApproveTest} style={{marginBottom: "10px"}}>
                         Test Pay Order
-                      </Button>
+                      </Button>*/}
                       <div>
                         <PayPalButtons createOrder={createOrder} onApprove={onApprove}
                                        onError={onError}>
