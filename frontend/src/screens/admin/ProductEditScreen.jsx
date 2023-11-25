8691583123cac4d73902d77566f480dfae7d170b
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-24 22:00:10 -0700

    Section 11: Upload product image - frontend

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -5,7 +5,11 @@ import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import FormContainer from "../../components/FormContainer";
 import {toast} from "react-toastify";
-import {useUpdateProductMutation, useGetProductDetailsQuery} from "../../slices/productsApiSlice";
+import {
+    useUpdateProductMutation,
+    useGetProductDetailsQuery,
+    useUploadProductImageMutation,
+} from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
     const {id: productId} = useParams();
@@ -21,6 +25,8 @@ const ProductEditScreen = () => {
     const {data: product, isLoading, refetch, error} = useGetProductDetailsQuery(productId);
 
     const [updateProduct, {isLoading: isLoadingUpdate}] = useUpdateProductMutation();
+
+    const [uploadProductImage, {isLoading: loadingUpload}] = useUploadProductImageMutation();
 
     const navigate = useNavigate();
 
@@ -47,7 +53,7 @@ const ProductEditScreen = () => {
             brand,
             category,
             countInStock,
-            description
+            description,
         };
 
         const result = await updateProduct(updatedProduct);
@@ -58,7 +64,21 @@ const ProductEditScreen = () => {
             toast.success("Product updated");
             navigate("/admin/productlist");
         }
-    }
+    };
+
+    const uploadFileHandler = async (e) => {
+        const formData = new FormData();
+
+        formData.append("image", e.target.files[0]);
+
+        try {
+            const res = await uploadProductImage(formData).unwrap();
+            toast.success(res.message);
+            setImage(res.image);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
 
     return (
         <>
@@ -87,7 +107,16 @@ const ProductEditScreen = () => {
                                           onChange={(e) => setPrice(e.target.value)}></Form.Control>
                         </Form.Group>
 
-                        {/* TODO: IMAGE INPUT PLACEHOLDER */}
+                        <Form.Group controlId="image" className="my-2">
+                            <Form.Label>Image</Form.Label>
+                            <Form.Control type="text" placeholder="Enter image url" value={image}
+                                          onChange={(e) => setImage}>
+
+                            </Form.Control>
+                            <Form.Control type="file" label="Choose file" onChange={uploadFileHandler}>
+
+                            </Form.Control>
+                        </Form.Group>
 
                         <Form.Group controlId="brand" className="my-2">
                             <Form.Label>Brand</Form.Label>
