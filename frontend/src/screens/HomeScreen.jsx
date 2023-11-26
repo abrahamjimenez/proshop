e401393ddf3f6098577a6acc5c201db71152669e
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-26 15:07:32 -0700

    Section 12: Product carousel

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,8 +3,9 @@ import {Link, useParams} from 'react-router-dom';
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import {useGetProductsQuery} from "../slices/productsApiSlice";
 import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import {useGetProductsQuery} from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
     const {pageNumber, keyword} = useParams();
@@ -13,7 +14,7 @@ const HomeScreen = () => {
 
     return (
         <>
-            {keyword && <Link to="/" className="btn btn-light mb-4">Go Back</Link>}
+            {!keyword ? <ProductCarousel/> : <Link to="/" className="btn btn-light mb-4">Go Back</Link>}
             {isLoading ? (
                 <Loader/>
             ) : error ? (
